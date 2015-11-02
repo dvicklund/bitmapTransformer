3fd661a8880f1300455ae23364c3be56d0bdc8f8
David Vicklund <david@vicklund.com>
2015-11-02 13:01:01 -0800

    compartmentalized different pieces

---
+++ index.js
@@ -0,0 +1,27 @@
+// Import filesystem module
+var fs = require('fs');
+var reader = require('./lib/readBitmap');
+var transformer = require('./lib/transformer');
+var args = process.argv;
+console.log(args);
+var buffs;
+var finalBuffer;
+
+// Read bitmap1.bmp to Buffer "bitmap"
+var bitmap = fs.readFile('bitmap1.bmp', function(error, data){
+  if (error) console.log(error);
+  buffs = reader.readBitmap(data);
+
+  // Run a transform
+  
+
+  finalBuffer = Buffer.concat(buffs);
+
+  // Finally, save the output to an external file
+  fs.writeFile('bitmap2.bmp', finalBuffer, function(err) {
+    if (err) console.log(err);
+    console.log("Saved");
+  });
+});
+
+
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var mocha = require('mocha');
+var chai = require('chai');
+
+describe("index.js", function() {
+  
+}); 
